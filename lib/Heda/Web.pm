690d6a0d82632d1f50d418399118dd2e54386ac2
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-09-11 12:59:26 +0900

    implement supervisor login/logout and basic user list

--- lib/Heda/Web.pm
+++ lib/Heda/Web.pm
@@ -60,7 +60,6 @@ filter 'check_supervisor_login' => sub {
         if ($session->get('authenticated') and $session->get('supervisor')) {
             $c->stash->{supervisor} = $session->get('supervisor');
             $c->stash->{username} = $session->get('username');
-            return;
         }
         $c->stash->{session} = $session;
         $session->response_filter($c->res);
@@ -76,8 +75,7 @@ filter 'require_supervisor_login' => sub {
         my $session = $self->session($c->req);
 
         unless ($session->get('authenticated') and $session->get('supervisor')) {
-            $c->halt(401, 'specified operations requires login, see /.');
-            return;
+            return $c->halt(401, 'specified operations requires login, see /.');
         }
         $c->stash->{supervisor} = $session->get('supervisor');
         $c->stash->{username} = $session->get('username');
@@ -173,17 +171,75 @@ post '/authenticate' => [qw/check_supervisor_login/] => sub {
 };
 
 get '/login' => [qw/check_supervisor_login/] => sub {
-    
+    my ( $self, $c ) = @_;
+
+    if ($c->stash->{supervisor}) { # already logged in as supervisor
+        return $c->redirect('/list');
+    }
+
+    my $errors = $c->stash->{session}->get('autherrors');
+    $errors ||= +{
+        username => +{ flag => 0 },
+        password => +{ flag => 0 },
+    };
+    $c->render('login.tx', { autherrors => $errors });
 };
 
 post '/login' => [qw/check_supervisor_login/] => sub {
-    
+    my ( $self, $c ) = @_;
+    my $username = $c->req->param('username');
+    my $password = $c->req->param('password');
+
+    my $user = $self->users->authenticate( username => $username, password => $password );
+    my $errors = {};
+    unless ($user and $user->{superuser}) {
+        $errors->{password} = +{ flag => 1, message => 'Incorrect password' } unless $user;
+        $errors->{username} = +{ flag => 1, message => 'You are NOT supervisor' } unless $user and $user->{supervisor};
+        $c->stash->{session}->set('autherrors', $errors);
+        return $c->redirect('/login');
+    }
+
+    $c->stash->{session}->set('authenticated', 1);
+    $c->stash->{session}->set('supervisor', 1);
+    $c->redirect('/list');
 };
 
 get '/logout' => [qw/require_supervisor_login/] => sub {
+    my ( $self, $c ) = @_;
+    $c->stash->{session}->remove('authenticated');
+    $c->stash->{session}->remove('supervisor');
+    $c->redirect('/');
 };
 
 get '/list' => [qw/require_supervisor_login/] => sub {
+    my ( $self, $c ) = @_;
+    my $list = $self->users->all();
+
+    my $sort = $c->req->param('s') || 'u';
+    my $order = $c->req->param('o') || 'a';
+
+    if ($sort eq 'u') { #username
+        $list = [sort { $a->{username} cmp $b->{username} } @$list];
+    } elsif ($sort eq 's') { #subid
+        $list = [sort { $a->{subid} cmp $b->{subid} } @$list];
+    } elsif ($sort eq 'e') { #email address
+        $list = [sort { $a->{mailaddress} cmp $b->{mailaddress} } @$list];
+    } elsif ($sort eq 'm') { #modified-at
+        $list = [sort { $a->{modified_at} cmp $b->{modified_at} } @$list];
+    }
+
+    if ($order eq 'd') { #desc
+        $list = [reverse @$list];
+    } else { # 'a' or blank: asc
+        # nothing to do
+    }
+
+    foreach my $user (@$list) {
+        my $account = decode_json($user->{accounts});
+        $user->{account_list} = [map { [$_, $account->{$_}] } sort(keys %$account)];
+    }
+
+    $c->render('list.tx', { list => $list, sort => $sort, order => $order });
 };
 
 get '/create' => [qw/require_supervisor_login/] => sub {
